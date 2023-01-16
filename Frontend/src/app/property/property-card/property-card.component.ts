ece739cfcb047ed796418838de82424f6bdb7459
Asif Mahmud <asif.mahmud@soti.net>
2023-01-16 11:42:01 -0500

    added card

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -10,5 +10,10 @@ import { Component } from "@angular/core";
     }
 )
 export class PropertyCardComponent {
-
+    Property: any = {
+        "Id": 1,
+        "Name": "Birla House",
+        "Type": "House",
+        "Price" : 1200
+    }
 }
