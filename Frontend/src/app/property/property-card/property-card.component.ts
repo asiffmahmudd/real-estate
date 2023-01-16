de0cc14fd2687849cd1ef6a10c5024a0482e9ef6
Asif Mahmud <asif.mahmud@soti.net>
2023-01-16 14:34:42 -0500

    added hover effect on card

--- Frontend/src/app/property/property-card/property-card.component.ts
+++ Frontend/src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component(
     {
@@ -10,10 +10,5 @@ import { Component } from "@angular/core";
     }
 )
 export class PropertyCardComponent {
-    Property: any = {
-        "Id": 1,
-        "Name": "Birla House",
-        "Type": "House",
-        "Price" : 1200
-    }
+    @Input() property: any
 }
--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit{
+  properties: Array<any> = [
+    {
+      "Id": 1,
+      "Name": "Birla House",
+      "Type": "House",
+      "Price" : 1200
+    },
+    {
+      "Id": 2,
+      "Name": "Marla House",
+      "Type": "House",
+      "Price" : 1000
+      },
+    {
+      "Id": 3,
+      "Name": "Greenleaf",
+      "Type": "House",
+      "Price" : 1100
+    },
+    {
+      "Id": 4,
+      "Name": "Ruby",
+      "Type": "House",
+      "Price" : 1300
+      },
+  ]
 
+  constructor(
+
+  ){}
+
+  ngOnInit(): void {
+    
+  }
 }
